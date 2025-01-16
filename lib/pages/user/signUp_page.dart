01b76a9584fac94c8991a59db47f3d97d2edbfb0
SylvestreZodyorida <sylvestreahlonsou88@gmail.com>
2025-01-16 19:51:55 +0100

    cvcv

--- lib/pages/user/signUp_page.dart
+++ lib/pages/user/signUp_page.dart
@@ -1,7 +1,9 @@
 import 'package:fg_by_zodyy/pages/user/emailSignUp_page.dart';
 import 'package:fg_by_zodyy/pages/user/login_page.dart';
+import 'package:fg_by_zodyy/pages/user/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
 import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Import de la classe générée par l10n
 import 'package:fg_by_zodyy/main.dart'; // Import du gestionnaire de langues
 
@@ -16,67 +18,76 @@ class _SignUpPageState extends State<SignUpPage> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   // Fonction d'inscription avec Google
-  Future<void> _signUpWithGoogle() async {
+  Future<void> _signInWithGoogle() async {
     try {
-      await _googleSignIn.signIn();
-      // Une fois l'inscription réussie, vous pouvez rediriger vers une autre page
-      // Par exemple : Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return;
+      }
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      final User? user = userCredential.user;
+      if (user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const ProfilePage()),
+        );
+      }
     } catch (error) {
-      print(error);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erreur d\'authentification: $error')),
+      );
     }
   }
 
   // Fonction d'inscription avec Email
   void _signUpWithEmail() {
-    // Vous pouvez ici ajouter la logique pour vous inscrire avec un email (par exemple, utiliser Firebase Auth).
-    // Pour simplifier, on va juste rediriger vers une autre page.
-    // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const EmailSignUpPage()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // Récupérer la langue actuelle avec AppLocalizations
-    String currentLanguage = LanguageManager.getCurrentLanguage();
-    Locale currentLocale = Locale(currentLanguage); // Créez le Locale en fonction de la langue
-
-    // Vérifiez si la localisation est bien disponible
     final appLocalizations = AppLocalizations.of(context);
     if (appLocalizations == null) {
       return Scaffold(
-        body: const Center(child: CircularProgressIndicator()), // Affiche un indicateur de chargement si la localisation n'est pas disponible
+        body: const Center(child: CircularProgressIndicator()),
       );
     }
 
     return Scaffold(
-      // Pas d'AppBar
       body: Stack(
         children: [
-          // Image d'arrière-plan, ajustée pour couvrir tout l'écran
           Positioned.fill(
             child: Image.asset(
-              'assets/images/fond.png', // Votre image d'arrière-plan ici
+              'assets/images/fond.png',
               fit: BoxFit.cover,
             ),
           ),
-          // Le contenu de la page
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start, // Rapproche les éléments vers le haut
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const SizedBox(height: 140), // Réduit l'espacement entre le haut de l'écran et le logo
-                // Envelopper l'image du logo avec ClipRRect pour les bords arrondis
+                const SizedBox(height: 140),
                 ClipRRect(
-                  borderRadius: BorderRadius.circular(90), // Définir le rayon d'arrondi ici
+                  borderRadius: BorderRadius.circular(90),
                   child: Image.asset(
                     'assets/images/rounded_logo.png',
-                    height: 100, // Taille du logo
+                    height: 100,
                   ),
                 ),
-                const SizedBox(height: 10), // Espace entre le logo et le texte
+                const SizedBox(height: 10),
                 Text(
-                  appLocalizations.sign_up, // Traduction pour "S'inscrire"
+                  appLocalizations.sign_up,
                   style: const TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
@@ -84,9 +95,9 @@ class _SignUpPageState extends State<SignUpPage> {
                   ),
                   textAlign: TextAlign.center,
                 ),
-                const SizedBox(height: 80), // Espacement entre le texte et les boutons
+                const SizedBox(height: 80),
                 ElevatedButton(
-                  onPressed: _signUpWithGoogle,
+                  onPressed: _signInWithGoogle,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromARGB(255, 255, 255, 255),
                     padding: const EdgeInsets.symmetric(vertical: 15),
@@ -94,23 +105,22 @@ class _SignUpPageState extends State<SignUpPage> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(Icons.account_circle, color: Colors.white),
+                      SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Image.asset('assets/images/google_logo.png'),
+                      ),
                       const SizedBox(width: 10),
                       Text(
-                        appLocalizations.sign_up_with_google, // Traduction pour "S'inscrire avec Google"
+                        appLocalizations.sign_up_with_google,
                         style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 20), // Espacement entre les boutons
+                const SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const EmailSignUpPage()),
-                    );
-                  },
+                  onPressed: _signUpWithEmail,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromARGB(255, 1, 14, 112),
                     padding: const EdgeInsets.symmetric(vertical: 15),
@@ -121,17 +131,16 @@ class _SignUpPageState extends State<SignUpPage> {
                       const Icon(Icons.email, color: Colors.white),
                       const SizedBox(width: 10),
                       Text(
-                        appLocalizations.sign_up_with_email, // Traduction pour "S'inscrire avec Email"
+                        appLocalizations.sign_up_with_email,
                         style: const TextStyle(color: Colors.white),
                       ),
                     ],
                   ),
                 ),
                 const SizedBox(height: 180),
-                // Ajout du texte "Pas encore de compte ?"
                 Center(
                   child: Text(
-                    appLocalizations.already_a_member, // Texte "Pas encore de compte ?"
+                    appLocalizations.already_a_member,
                     style: TextStyle(
                       fontSize: 16,
                       fontWeight: FontWeight.w600,
@@ -139,21 +148,19 @@ class _SignUpPageState extends State<SignUpPage> {
                     ),
                   ),
                 ),
-                const SizedBox(height: 10), // Espacement entre le texte et le bouton
-                // Ajout du bouton "S'inscrire ici"
+                const SizedBox(height: 10),
                 Center(
                   child: InkWell(
                     onTap: () {
-                      // Navigation vers la page de connexion
                       Navigator.pushReplacement(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => const LoginPage(), // Redirection vers la page de connexion
+                          builder: (context) => const LoginPage(),
                         ),
                       );
                     },
                     child: Text(
-                      appLocalizations.login, // Texte pour le bouton "Se connecter ici"
+                      appLocalizations.login,
                       style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.w600,
